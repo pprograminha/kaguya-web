1c8a6c9c0c56f40a45eca16411ca3d5c68ac3366
Tiaguin061 <tiaguin180@gmail.com>
2022-09-02 15:25:06 -0300

    feat: Added header  type on page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { Header } from '../components/Header';
 
 const Home: NextPage = () => {
 
@@ -8,6 +9,11 @@ const Home: NextPage = () => {
       <Head>
         <title>Kaguya - Home</title>
       </Head>
+      <Header
+        headerType={{
+          hasSignInButton: true
+        }}
+      />
     </>
   )
 }
