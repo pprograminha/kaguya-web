22509850bc42af7920381ee4796cdfb3d101d6c6
Tiaguin061 <tiaguin180@gmail.com>
2022-09-03 14:39:32 -0300

    improve: Removed css default on input

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -30,20 +30,11 @@ export function Input({
         {icon}
         <ChakraInput
           name={name}
-          border="none"
           size="lg"
           fontSize={["sm", "md"]}
           textColor="gray.300"
           color="gray.300"
-          _focusVisible={{
-            border: 'none'
-          }}
-          _focusWithin={{
-            border: 'none'
-          }}
-          _hover={{
-            border: 'normal'
-          }}
+          border="0"
           {...rest}
         />
       </Flex>
