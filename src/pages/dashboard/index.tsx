677bfc60069bab6efbd5b5d6d57970902abe6f36
Tiaguin061 <tiaguin180@gmail.com>
2022-09-02 23:37:28 -0300

    feat: Created dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,21 +1,65 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex, HStack, useBreakpointValue } from '@chakra-ui/react';
 import Head from 'next/head';
+
+import { LastLessonViewed } from '../../components/Dashboard/LastLessonViewed';
+import { MyTrails } from '../../components/Dashboard/MyTrails';
+import { OthersTrails } from '../../components/Dashboard/OthersTrails';
+import { Welcome } from '../../components/Dashboard/Welcome';
+
 import { Header } from '../../components/Header';
 
 export default function Dashboard() {
+  const isWideVersion = useBreakpointValue({ 
+    base: false,
+    "2xl": true
+  });
+
   return (
     <>
       <Head>
         <title>Kaguya | Dashboard</title>
       </Head>
 
-      <Flex>
+      <HStack flexDirection="column">
         <Header
           headerType={{
             hasUserProfile: true
           }}
         />
-      </Flex>
+
+        <Box
+          maxW={1480}
+          w="100%"
+          ml="0 !important"
+
+          display="flex"
+          flexDirection={!isWideVersion ? "column" : "row"}
+          px={["0", "2", "8"]}
+        >
+          <Box
+            w="100%"
+            maxW={[480, 860]}
+          >
+            <Flex
+              as="section"
+              flexDirection="column"
+              alignItems="flex-start"
+
+              mt="8"
+              p={["8"]}
+              w="100%"
+            >
+              <Welcome />
+              
+              <LastLessonViewed />
+            </Flex>
+            
+            <MyTrails />
+          </Box>
+
+          <OthersTrails />
+        </Box>
+      </HStack>
     </>
   )
 }
