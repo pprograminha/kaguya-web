993bc54cc93e5e21ebc0cdeb535e51bdf6e6ef13
Tiaguin061 <tiaguin180@gmail.com>
2022-09-03 21:50:14 -0300

    feat: Using next link

--- src/components/Dashboard/MyTrails/Trail.tsx
+++ src/components/Dashboard/MyTrails/Trail.tsx
@@ -1,50 +1,54 @@
 import { GridItem, Button, Image, Text } from '@chakra-ui/react';
-import { Progress } from '../../Progress';
+import NextLink from 'next/link';
 import Lordicon from '../../ReactLordicon';
+import { Progress } from '../../Progress';
 
 export function Trail() {
   return (
     <>
-      <GridItem
-        bg="blackAlpha.700"
-        py="8"
-        px="6"
-        display="flex"
-        alignItems="center"
-        flexDirection="column"
-        position="relative"
-        borderRadius="md"
-        cursor="pointer"
-      >
-        <Button
-          position="absolute"
-          top="2"
-          right="2"
-          bg="blackAlpha.600"
-          _hover={{
-            bg:"normal",
-            opacity: "0.7"
-          }}
+      <NextLink href="/trail/html">
+        <GridItem
+          as="a"
+          bg="blackAlpha.700"
+          py="8"
+          px="6"
+          display="flex"
+          alignItems="center"
+          flexDirection="column"
+          position="relative"
+          borderRadius="md"
+          cursor="pointer"
         >
-          <Lordicon icon="trash"/>
-        </Button>
-        <Image 
-          src="https://app-kaguya.s3.amazonaws.com/1096b9782cc48ca7b1b19422cfe39f-html5.png"
-          alt="html"
+          <Button
+            position="absolute"
+            top="2"
+            right="2"
+            bg="blackAlpha.600"
+            _hover={{
+              bg:"normal",
+              opacity: "0.7"
+            }}
+          >
+            <Lordicon icon="trash"/>
+          </Button>
+          <Image 
+            src="https://app-kaguya.s3.amazonaws.com/1096b9782cc48ca7b1b19422cfe39f-html5.png"
+            alt="html"
 
-          w={["14","16"]}
-          h={["14","16"]}
-        />
-        <Text
-          fontSize={["sm", "md", "xl"]}
-          fontWeight="bold"
-          letterSpacing="wider"
-          mt="3"
-        >
-          HTML
-        </Text>
-        <Progress percent={79} />
-      </GridItem>
+            w={["14","16"]}
+            h={["14","16"]}
+          />
+          <Text
+            fontSize={["sm", "md", "xl"]}
+            fontWeight="bold"
+            letterSpacing="wider"
+            mt="3"
+          >
+            HTML
+          </Text>
+          <Progress percent={79} />
+        </GridItem>
+      </NextLink>
     </>
   );
 }
