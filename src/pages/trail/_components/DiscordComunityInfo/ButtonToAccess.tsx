4a1ce807a8870c97db105e41bd9ae5fd355995ea
Tiaguin061 <tiaguin180@gmail.com>
2022-09-05 11:32:25 -0300

    feat: Created section to discord comunity info in trail page

---
+++ src/pages/trail/_components/DiscordComunityInfo/ButtonToAccess.tsx
@@ -0,0 +1,34 @@
+import {
+  Link as ChakraLink
+} from '@chakra-ui/react';
+import { BsDiscord } from 'react-icons/bs';
+
+export function DiscordButtonToAccess() {
+  return (
+    <>
+      <ChakraLink
+        href="https://discord.gg/3wdCtmMTSx" 
+        target="_blank"
+
+        bg="#5865f2"
+        color="white"
+        display="flex"
+        alignItems="center"
+        gap="2"
+        py="2"
+        px="4"
+        mt="4"
+        borderRadius="md"
+        cursor="pointer"
+        w="max"
+        transition="all 0.2s"
+        _hover={{
+          filter: "brightness(110%)"
+        }}
+      >
+        <BsDiscord/>
+        Discord
+      </ChakraLink>
+    </>
+  )
+}
