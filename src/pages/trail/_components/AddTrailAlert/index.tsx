3d07523fa6d9c440bb965e0f910943856a9e7e8e
Tiaguin061 <tiaguin180@gmail.com>
2022-09-05 11:33:41 -0300

    feat: Created component to trail alert in page

---
+++ src/pages/trail/_components/AddTrailAlert/index.tsx
@@ -0,0 +1,12 @@
+import { Flex } from '@chakra-ui/react';
+import { Content } from './Content';
+
+export function AddTrailAlert() {
+  return (
+    <>
+     <Flex>
+        <Content />
+      </Flex>
+    </>
+  );
+}
