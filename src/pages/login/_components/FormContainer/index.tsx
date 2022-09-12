080fbc4f4215472150a5ba58f4b48cb68238bc81
Tiaguin061 <tiaguin180@gmail.com>
2022-09-12 13:11:08 -0300

    feat: Created logic to user signIn

--- src/pages/login/_components/FormContainer/index.tsx
+++ src/pages/login/_components/FormContainer/index.tsx
@@ -1,15 +1,59 @@
-import { Box, Flex, Text, VStack } from '@chakra-ui/react';
+import { Box, Flex, Text, useToken, VStack } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import { FaLock } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 
 import { SignInWithGithubButton } from '../SignInButtons/SignInWithGithubButton';
 import { SignInWithGoogleButton } from '../SignInButtons/SignInWithGoogleButton';
 import { ForgotPasswordLink } from './ForgotPasswordLink';
 import { FormHeader } from './FormHeader';
-import { FormInputs } from './Inputs';
 
 import { Button } from '@/components/Button';
 import { DividerLine } from '@/components/DividerLine';
+import { Input } from '@/components/Form/Input';
+import { InputPassword } from '@/components/Form/InputPassword';
+
+import { User } from 'services/kaguya/types';
+import { useAuth } from 'hooks/useAuth';
+
+export type LoginResponse = {
+  user: User;
+  token: string;
+}
+
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string()
+    .required('E-mail obrigatório')
+    .email('E-mail inválido'),
+  password: yup.string()
+    .required('Senha obrigatória'),
+});
 
 export function FormContainer() {
+  const [gray300]= useToken("colors", [
+    'gray.300', 
+  ]);
+
+  const { signIn } = useAuth();
+
+  const { formState, handleSubmit, register } = useForm<SignInFormData>({
+    resolver: yupResolver(signInFormSchema),
+  });
+
+  const signInErrors = formState.errors;
+  
+  const handleSignIn: SubmitHandler<SignInFormData> = async (data) => {
+    await signIn(data);
+  }
+
   return (
     <>
       <Box
@@ -17,6 +61,7 @@ export function FormContainer() {
         w="100%"
         pb="12"
         as="form"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <FormHeader />
         <VStack
@@ -35,10 +80,30 @@ export function FormContainer() {
           <DividerLine />
         </Flex>
         
-        <FormInputs />
+        <VStack
+          spacing="3"
+        >
+          <Input
+            placeholder="E-mail"
+            icon={<MdEmail color={`${gray300}`} />}
+            error={signInErrors.email}
+            {...register('email')}
+          />
+          <InputPassword
+            placeholder="Senha"
+            icon={<FaLock color={`${gray300}`} />}
+            error={signInErrors.password}
+            {...register('password')}
+          />
+        </VStack>
         <ForgotPasswordLink />
         
-        <Button mt="2" w="100%" type="submit" disabled>
+        <Button
+          mt="2"
+          w="100%"
+          isLoading={formState.isSubmitting}
+          type="submit"
+        >
           Entrar
         </Button>
       </Box>
