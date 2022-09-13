285f0606ff68edd525b41fc436d9ca14e26a5baa
Tiaguin061 <tiaguin180@gmail.com>
2022-09-13 12:46:03 -0300

    feat: Separeted kaguya api client

--- src/services/kaguya/api.ts
+++ src/services/kaguya/api.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
+import { GetServerSidePropsContext } from 'next';
+import { parseCookies } from 'nookies';
 
-export const kaguyaApi = axios.create({
-  baseURL: 'https://kaguya-it.herokuapp.com',
-});
+export const tokenCookieKey = 'kaguyaApp.token';
+
+export function setupAPIClient(ctx: GetServerSidePropsContext | undefined = undefined) {
+  let cookies = parseCookies(ctx);
+  
+  const kaguyaApi = axios.create({
+    baseURL: 'https://kaguya-it.herokuapp.com',
+  });
+  
+  kaguyaApi.defaults.headers.common.Authorization = `Bearer ${cookies[tokenCookieKey]}`;
+
+  return kaguyaApi;
+}
