1472467e27abb65c05559afae5caf30291ad9705
Tiaguin061 <tiaguin180@gmail.com>
2022-09-13 19:50:25 -0300

    feat: Consum my trail info

--- src/pages/dashboard/_components/MyTrails/Trail.tsx
+++ src/pages/dashboard/_components/MyTrails/Trail.tsx
@@ -1,54 +1,99 @@
-import { GridItem, Button, Image, Text } from '@chakra-ui/react';
+import { 
+  GridItem, 
+  Button, 
+  Image, 
+  Text,
+  Box,
+  useDisclosure, 
+} from '@chakra-ui/react';
 import NextLink from 'next/link';
-import Lordicon from '../../ReactLordicon';
-import { Progress } from '../../Progress';
 
-export function Trail() {
+import Lordicon from '@/components/ReactLordicon';
+import { Progress } from '@/components/Progress';
+import { ConfirmRemoveTrailModal } from './ConfirmRemoveTrailModal';
+
+interface UserTrail {
+  id: string;
+  name: string;
+  slug: string;
+  avatar_url: string;
+  
+  user_trail: {
+    progress: number;
+    enabled: boolean;
+  }
+}
+
+interface TrailProps {
+  trail: UserTrail;
+}
+
+export function Trail({
+  trail
+}: TrailProps) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
-      <NextLink href="/trail/html">
-        <GridItem
-          as="a"
-          bg="blackAlpha.700"
-          py="8"
-          px="6"
-          display="flex"
-          alignItems="center"
-          flexDirection="column"
-          position="relative"
-          borderRadius="md"
-          cursor="pointer"
-        >
-          <Button
-            position="absolute"
-            top="2"
-            right="2"
-            bg="blackAlpha.600"
-            _hover={{
-              bg:"normal",
-              opacity: "0.7"
-            }}
-          >
-            <Lordicon icon="trash"/>
-          </Button>
-          <Image 
-            src="https://app-kaguya.s3.amazonaws.com/1096b9782cc48ca7b1b19422cfe39f-html5.png"
-            alt="html"
+      <Box
+        position="relative"
+      >
+        <Button
+          position="absolute"
+          top="2"
+          right="2"
+          bg="blackAlpha.600"
+          zIndex="10"
+          _hover={{
+            bg:"normal",
+            opacity: "0.7"
+          }}
 
-            w={["14","16"]}
-            h={["14","16"]}
-          />
-          <Text
-            fontSize={["sm", "md", "xl"]}
-            fontWeight="bold"
-            letterSpacing="wider"
-            mt="3"
+          onClick={onOpen}
+        >
+          <Lordicon icon="trash"/>
+        </Button>
+        <NextLink href={`/trail/${trail.slug}`}>
+          <GridItem
+            as="a"
+            bg="blackAlpha.700"
+            py="8"
+            px="6"
+            display="flex"
+            alignItems="center"
+            flexDirection="column"
+            borderRadius="md"
+            cursor="pointer"
           >
-            HTML
-          </Text>
-          <Progress percent={79} />
-        </GridItem>
-      </NextLink>
+            <Image 
+              src={trail.avatar_url}
+              alt={`Avatar da trilha de ${trail.name}`}
+
+              w={["14","16"]}
+              h={["14","16"]}
+            />
+            <Text
+              fontSize={["sm", "md", "xl"]}
+              fontWeight="bold"
+              letterSpacing="wider"
+              mt="3"
+            >
+              {trail.name}
+            </Text>
+            {trail.user_trail.enabled && (
+              <Progress percent={trail.user_trail.progress} />
+            )}
+          </GridItem>
+        </NextLink>
+      </Box>
+
+      <ConfirmRemoveTrailModal 
+        modal={{
+          isOpen,
+          onClose
+        }}
+        trail={trail}
+      />
     </>
   );
 }
