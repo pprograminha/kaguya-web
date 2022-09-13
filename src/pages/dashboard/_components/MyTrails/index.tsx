d29336351befbbd5a0d3f31c9debf3f17fda2053
Tiaguin061 <tiaguin180@gmail.com>
2022-09-13 19:50:10 -0300

    feat: Listing my trails

--- src/pages/dashboard/_components/MyTrails/index.tsx
+++ src/pages/dashboard/_components/MyTrails/index.tsx
@@ -1,6 +1,9 @@
-import { Box, Heading, keyframes } from '@chakra-ui/react';
+import { useAuth } from '@/hooks/useAuth';
+import { kaguyaApi } from '@/services/kaguya/apiClient';
+import { Box, CircularProgress, Grid, Heading, keyframes } from '@chakra-ui/react';
+import { useQuery } from 'react-query';
 import { MyTrailsNoContent } from './MyTrailsNoContent';
-// import { MyTrailsNoContent } from './MyTrailsNoContent';
+import { Trail } from './Trail';
 
 const animate = keyframes`
   from {  
@@ -13,7 +16,45 @@ const animate = keyframes`
   }
 `
 
+interface UserTrail {
+  id: string;
+  name: string;
+  slug: string;
+  description: string;
+  avatar_url: string;
+  
+  created_at: string;
+  updated_at: string;
+
+  _count: {
+    playlists: number;
+    users: number;
+    lessons: number;
+  };
+
+  user_trail: {
+    progress: number;
+    enabled: boolean;
+  }
+}
+
 export function MyTrails() {
+  const { user } = useAuth();
+
+  const { data, isFetching } = useQuery<UserTrail[]>('userTrails', async () => {
+    const response = await kaguyaApi.get<UserTrail[]>('/user-trails/list-all', {
+      params: {
+        user_id: user?.id,
+        take: 3,
+      }
+    });
+
+    return response.data;
+  }, {
+    staleTime: 1000 * 60 * 10 , // 60 minutes
+    
+  });
+
   return (
     <>
       <Box
@@ -24,20 +65,34 @@ export function MyTrails() {
         <Heading
           fontSize={["md", "lg", "2xl"]}
           mb="4"
+          gap="2"
+          display="flex"
+          alignItems="center"
         >
           Minhas trilhas
+          {isFetching && (
+            <CircularProgress
+              isIndeterminate
+              color='pink.800'
+              size={6}
+            />
+          )}
         </Heading>
 
-        <MyTrailsNoContent />
-        
-        {/* <Grid
-          gridTemplateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)"]}
-          gap="4"
-          overflowY="auto"
-          maxH={400}
-        >
-          <Trail />
-        </Grid> */}
+        {!data?.length ? (
+          <MyTrailsNoContent />
+        ) : (
+          <Grid
+            gridTemplateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)"]}
+            gap="4"
+            overflowY="auto"
+            maxH={400}
+          >
+            {data && data.map(trail => (
+              <Trail key={trail.id} trail={trail} />
+            ))}
+          </Grid>
+        )}
       </Box>
     </>
   );
