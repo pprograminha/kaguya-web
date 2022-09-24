824a218c2166acb5452629cfd14b7131872d66f1
Tiaguin061 <tiaguin180@gmail.com>
2022-09-24 17:07:54 -0300

    feat: Created footer and using

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,6 @@ import {
   Text,
   Link as ChakraLink,
   useToken,
-  Divider,
   useMediaQuery
 } from '@chakra-ui/react';
 import { useSize } from "@chakra-ui/react-use-size";
@@ -20,6 +19,7 @@ import { useRef } from 'react';
 import { DividerLine } from '@/components/DividerLine';
 import Lordicon from '@/components/ReactLordicon';
 import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
 
 import { useAuth } from '@/hooks/useAuth';
 
@@ -438,62 +438,7 @@ export default function HomePage() {
         </Box>
       </Box>
 
-      <Box
-        as="footer"
-        bg="blackAlpha.800"
-        p="8"
-      >
-        <Flex
-          w="100%"
-          maxW={1480}
-          mx="auto"
-          justifyContent="space-between"
-          flexDirection={!isLargerThan768 ? "column" : "row"}
-          gap={["4", "0"]}
-        >
-          <Box>
-            <Text
-              color="gray.300"
-              as="span"
-              fontSize={["md"]}
-              display="flex"
-              alignItems="center"
-              gap="6"
-            >
-              Kaguya - 2022
-            </Text>
-          </Box>
-
-          <Flex
-            gap={["0.5", "0.5", "4"]}
-            flexDirection={!isLargerThan768 ? "column" : "row"}
-          >
-            <NextLink href="/terms" passHref>
-              <ChakraLink
-                color="gray.300"
-              >
-                Termos de uso
-              </ChakraLink>
-            </NextLink>
-            <Divider orientation='vertical' />
-            <NextLink href="/privacy-policy" passHref>
-              <ChakraLink
-                color="gray.300"
-              >
-                Politica de privacidade
-              </ChakraLink>
-            </NextLink>
-            <Divider orientation='vertical' />
-            <NextLink href="/help" passHref>
-              <ChakraLink
-                color="gray.300"
-              >
-                Ajuda
-              </ChakraLink>
-            </NextLink>
-          </Flex>
-        </Flex>
-      </Box>
+      <Footer />
     </>
   )
 }
