c8a8b38b2ced62d62666214095230fabe795a7bb
Tiaguin061 <tiaguin180@gmail.com>
2022-09-29 13:01:30 -0300

    fix: Using logo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link rel="shortcut icon" href="assets/imgs/logo.png" />
+          <link rel="shortcut icon" href="/assets/imgs/logo.png" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet" />
 		      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet" />
           <script async src="https://cdn.lordicon.com/xdjxvujz.js" ></script>
