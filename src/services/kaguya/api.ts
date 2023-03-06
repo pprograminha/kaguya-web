97a43b3eb610dee1c5cf4a572b03bfb8e6132c00
Tiaguin061 <tiaguin180@gmail.com>
2023-03-06 17:57:05 -0300

    feat: Added url to production

--- src/services/kaguya/api.ts
+++ src/services/kaguya/api.ts
@@ -1,5 +1,5 @@
-import axios from 'axios';
 import { GetServerSidePropsContext } from 'next';
+import axios from 'axios';
 import { parseCookies } from 'nookies';
 
 export const tokenCookieKey = 'kaguyaApp.token';
@@ -7,8 +7,10 @@ export const tokenCookieKey = 'kaguyaApp.token';
 export function setupAPIClient(ctx: GetServerSidePropsContext | undefined = undefined) {
   let cookies = parseCookies(ctx);
   
+  const baseURL = process.env.NODE_ENV === 'production' && process.env.KAGUYA_API_BASE_URL;
+  
   const kaguyaApi = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: baseURL || 'http://localhost:3333',
   });
   
   kaguyaApi.defaults.headers.common.Authorization = `Bearer ${cookies[tokenCookieKey]}`;
