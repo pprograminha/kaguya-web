841f5be27b8d90e1e74b06041e4709ec1d166aea
Tiaguin061 <tiaguin180@gmail.com>
2023-03-06 18:25:22 -0300

    refactor: Correct url to production

--- src/services/kaguya/api.ts
+++ src/services/kaguya/api.ts
@@ -7,10 +7,10 @@ export const tokenCookieKey = 'kaguyaApp.token';
 export function setupAPIClient(ctx: GetServerSidePropsContext | undefined = undefined) {
   let cookies = parseCookies(ctx);
   
-  const baseURL = process.env.NODE_ENV === 'production' && process.env.KAGUYA_API_BASE_URL;
+  const baseURL = process.env.NODE_ENV === 'production' ? 'https://platform-backend-production.up.railway.app' : 'http://localhost:3333';
   
   const kaguyaApi = axios.create({
-    baseURL: baseURL || 'http://localhost:3333',
+    baseURL,
   });
   
   kaguyaApi.defaults.headers.common.Authorization = `Bearer ${cookies[tokenCookieKey]}`;
