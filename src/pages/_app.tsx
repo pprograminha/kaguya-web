0e6ce970e5ed0195642081e53b3daa26781946ce
Tiaguin061 <tiaguin180@gmail.com>
2023-03-06 17:57:22 -0300

    refactor: Organized imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
-import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react'
-import { QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-
-import { theme } from '../styles/theme';
-import { AuthProvider } from 'contexts/AuthContext';
-import { queryClient } from '@/services/reactQueryClient';
+import type { AppProps } from "next/app";
+import { AuthProvider } from "contexts/AuthContext";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { queryClient } from "@/services/reactQueryClient";
+import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -19,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <ReactQueryDevtools />
       </QueryClientProvider>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
