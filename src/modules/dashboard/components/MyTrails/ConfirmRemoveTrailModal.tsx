c62353e7d4b68630cac8ce4c583e8161747d4440
Tiaguin061 <tiaguin180@gmail.com>
2023-03-06 22:06:10 -0300

    refactor: Changed remove user trail api method

--- src/modules/dashboard/components/MyTrails/ConfirmRemoveTrailModal.tsx
+++ src/modules/dashboard/components/MyTrails/ConfirmRemoveTrailModal.tsx
@@ -1,112 +1,113 @@
-import { 
-  Button, 
-  Text,
-  useToast, 
-
+import {
+  Button,
   Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
   ModalBody,
+  ModalCloseButton,
+  ModalContent,
   ModalFooter,
-} from '@chakra-ui/react';
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 
-import { kaguyaApi } from '@/services/kaguya/apiClient';
-import { apiError } from '@/utils/apiFormatError';
-import { queryClient } from '@/services/reactQueryClient';
+import { apiError } from "@/utils/apiFormatError";
+import { kaguyaApi } from "@/services/kaguya/apiClient";
+import { queryClient } from "@/services/reactQueryClient";
 
-interface UserTrail {
+interface TrailData {
   id: string;
   name: string;
+  slug: string;
+  avatar_url: string;
+
+  user_trail: {
+    progress: number;
+    enabled: boolean;
+  };
 }
 
 interface ConfirmRemoveTrailModalProps {
-  trail: UserTrail;
+  trail: TrailData;
   modal: {
     isOpen: boolean;
     onClose: () => void;
-  }
+  };
 }
 
 export function ConfirmRemoveTrailModal({
   trail,
-  modal
+  modal,
 }: ConfirmRemoveTrailModalProps) {
   const toast = useToast();
-  
+
   async function removeUserTrail() {
     try {
-      await kaguyaApi.delete('/user-trails', {
-        params: {
-          trail_id: trail.id
-        }
+      await kaguyaApi.patch("/user-trails/change-enabled", {
+        trail_id: trail.id,
       });
 
-      await queryClient.invalidateQueries('userTrails');
-      await queryClient.invalidateQueries('othersTrails');
+      await queryClient.invalidateQueries("userTrails");
+      await queryClient.invalidateQueries("othersTrails");
+      await queryClient.invalidateQueries(["playlistsFromTrail", trail?.slug]);
 
       modal.onClose();
 
       toast({
-        title: 'Trilha removida',
+        title: "Trilha removida",
         description: `Você removeu a trilha de ${trail.name} de sua conta.`,
-        status: 'info',
+        status: "info",
         duration: 5000,
         isClosable: true,
-        position: 'top-right',
+        position: "top-right",
       });
     } catch (error) {
       const errors = apiError(error);
 
-      errors.messages.forEach(messageError => {
+      errors.messages.forEach((messageError) => {
         toast({
-          title: 'Erro na remoção de trilha',
+          title: "Erro na remoção de trilha",
           description: messageError,
-          status: 'error',
+          status: "error",
           duration: 6000,
           isClosable: true,
-          position: 'top-right',
+          position: "top-right",
         });
-      })
+      });
     }
   }
 
   return (
     <>
       <Modal isOpen={modal.isOpen} onClose={modal.onClose} isCentered>
-        <ModalOverlay
-          bg="blackAlpha.700"
-          opacity="0.6 !important"
-        />
-        <ModalContent
-          bg="blackAlpha.600"
-          alignItems="flex-start"
-          py="4"
-          mx="4"
-        >
+        <ModalOverlay bg="blackAlpha.700" opacity="0.6 !important" />
+        <ModalContent bg="blackAlpha.600" alignItems="flex-start" py="4" mx="4">
           <ModalHeader fontSize={["md", "lg"]}>Remoção de trilha</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
             <Text fontSize={["sm", "md"]}>
-              A remoção desta trilha é totalmente reversível e a qualquer momento você poderá reativar e continuar estudando.
+              A remoção desta trilha é totalmente reversível e a qualquer
+              momento você poderá reativar e continuar estudando.
             </Text>
-            <br/>
+            <br />
             <Text fontSize={["sm", "md"]}>
-              Você tem certeza que quer remover a trilha de 
+              Você tem certeza que quer remover a trilha de
               <Text
                 color="pink.500"
                 display="inline"
                 fontWeight="bold"
                 fontSize={["sm", "md"]}
-              > {trail.name} </Text> 
+              >
+                {" "}
+                {trail.name}{" "}
+              </Text>
               da sua conta?
             </Text>
           </ModalBody>
 
           <ModalFooter>
             <Button
-              colorScheme='pink'
+              colorScheme="pink"
               mr={3}
               onClick={modal.onClose}
               fontSize={["sm", "md"]}
@@ -114,7 +115,7 @@ export function ConfirmRemoveTrailModal({
               Cancelar
             </Button>
             <Button
-              colorScheme='ghost'
+              colorScheme="ghost"
               onClick={removeUserTrail}
               fontSize={["sm", "md"]}
             >
@@ -124,5 +125,5 @@ export function ConfirmRemoveTrailModal({
         </ModalContent>
       </Modal>
     </>
-  )
+  );
 }
