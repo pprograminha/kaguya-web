e88e225ca838e954f159ac346233360d4d473e5c
Tiaguin061 <tiaguin180@gmail.com>
2023-03-06 22:05:16 -0300

    eslint: Default code pattern

--- src/contexts/AuthContext/index.tsx
+++ src/contexts/AuthContext/index.tsx
@@ -1,19 +1,18 @@
-import { setCookie, destroyCookie, parseCookies } from 'nookies';
-import Router from 'next/router';
-import { createContext, useEffect, useState } from 'react';
-
-import { kaguyaApi } from 'services/kaguya/apiClient';
-
-import { 
-  SignInCredentials, 
-  User, 
-  SignInResponse, 
+import {
   RegisterUserCredentials,
-  RegisterUserResponse
-} from './types';
-import { useToast } from '@chakra-ui/react';
-import { apiError } from 'utils/apiFormatError';
-import { tokenCookieKey } from '@/services/kaguya/api';
+  RegisterUserResponse,
+  SignInCredentials,
+  SignInResponse,
+  User,
+} from "./types";
+import { createContext, useEffect, useState } from "react";
+import { destroyCookie, parseCookies, setCookie } from "nookies";
+
+import Router from "next/router";
+import { apiError } from "utils/apiFormatError";
+import { kaguyaApi } from "services/kaguya/apiClient";
+import { tokenCookieKey } from "@/services/kaguya/api";
+import { useToast } from "@chakra-ui/react";
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
@@ -21,7 +20,7 @@ type AuthContextData = {
   setUser(fn: User | null | ((user: User | null) => User | null)): void;
   isAuthenticated: boolean;
   user: User | null;
-}
+};
 
 interface AuthProviderProps {
   children: React.ReactNode;
@@ -32,12 +31,10 @@ export const AuthContext = createContext({} as AuthContextData);
 export function signOut() {
   destroyCookie(undefined, tokenCookieKey);
 
-  Router.push('/login');
+  Router.push("/login");
 }
 
-export function AuthProvider({
-  children
-}: AuthProviderProps) {
+export function AuthProvider({ children }: AuthProviderProps) {
   const toast = useToast();
 
   const [user, setUser] = useState<User | null>(null);
@@ -46,7 +43,7 @@ export function AuthProvider({
 
   async function getUser() {
     try {
-      const response = await kaguyaApi.get<User>('/profile');
+      const response = await kaguyaApi.get<User>("/profile");
       setUser(response.data);
     } catch (error) {
       signOut();
@@ -55,103 +52,108 @@ export function AuthProvider({
 
   useEffect(() => {
     const cookies = parseCookies();
-    
+
     const token = cookies[tokenCookieKey];
-    
-    if(token) {
+
+    if (token) {
       getUser();
     }
   }, []);
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
-      const response = await kaguyaApi.post<SignInResponse>('/sessions', {
-        email,
-        password
-      });
-  
-      const { token, user } = response.data;
-
-      setCookie(undefined, tokenCookieKey, token, {
-        maxAge: 60 * 60 * 24 * 30, // 30 days
-        path: '/'
-      });
-
-      setUser(user);
-
-      kaguyaApi.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-
-      toast({
-        title: 'Login feito',
-        description: "Bem vindo de volta.",
-        status: 'success',
-        duration: 5000,
-        isClosable: true,
-        position: 'top-right',
-      });
-
-      Router.push('/dashboard');
-    } catch (error) {
-      const errors = apiError(error);
-
-      errors.messages.forEach(messageError => {
-        toast({
-          title: 'Erro no login',
-          description: messageError,
-          status: 'error',
-          duration: 6000,
-          isClosable: true,
-          position: 'top-right',
-        });
-      })
-    }
-  }
-
-  async function signUp({ email, password, username }: RegisterUserCredentials) {
-    try {
-      const response = await kaguyaApi.post<RegisterUserResponse>('/users', {
+      const response = await kaguyaApi.post<SignInResponse>("/sessions", {
         email,
         password,
-        username
       });
-  
+
       const { token, user } = response.data;
 
       setCookie(undefined, tokenCookieKey, token, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
-        path: '/'
+        path: "/",
       });
 
       setUser(user);
 
-      kaguyaApi.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      kaguyaApi.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
       toast({
-        title: 'Registro feito com sucesso',
-        description: "Bem vindo a kaguya. Esperamos que seu ensino aqui seja excelente.",
-        status: 'success',
+        title: "Login feito",
+        description: "Bem vindo de volta.",
+        status: "success",
         duration: 5000,
         isClosable: true,
-        position: 'top-right',
+        position: "top-right",
       });
 
-      Router.push('/dashboard');
+      Router.push("/dashboard");
     } catch (error) {
       const errors = apiError(error);
 
-      errors.messages.forEach(messageError => {
+      errors.messages.forEach((messageError) => {
         toast({
-          title: 'Erro na criação de conta',
+          title: "Erro no login",
           description: messageError,
-          status: 'error',
+          status: "error",
           duration: 6000,
           isClosable: true,
-          position: 'top-right',
+          position: "top-right",
         });
-      })
+      });
     }
   }
-  
+
+  async function signUp({
+    email,
+    password,
+    username,
+  }: RegisterUserCredentials) {
+    try {
+      const response = await kaguyaApi.post<RegisterUserResponse>("/users", {
+        email,
+        password,
+        username,
+      });
+
+      const { token, user } = response.data;
+
+      setCookie(undefined, tokenCookieKey, token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+
+      setUser(user);
+
+      kaguyaApi.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+      toast({
+        title: "Registro feito com sucesso",
+        description:
+          "Bem vindo a kaguya. Esperamos que seu ensino aqui seja excelente.",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+
+      Router.push("/dashboard");
+    } catch (error) {
+      const errors = apiError(error);
+
+      errors.messages.forEach((messageError) => {
+        toast({
+          title: "Erro na criação de conta",
+          description: messageError,
+          status: "error",
+          duration: 6000,
+          isClosable: true,
+          position: "top-right",
+        });
+      });
+    }
+  }
+
   return (
     <>
       <AuthContext.Provider
@@ -167,5 +169,5 @@ export function AuthProvider({
         {children}
       </AuthContext.Provider>
     </>
-  )
+  );
 }
