04c9e4a249367535d098b25f4617009ed7b77f7f
pprograminha <marcosproenca144@gmail.com>
2023-05-06 11:16:02 -0300

    fix: services

--- src/services/kaguya/api.ts
+++ src/services/kaguya/api.ts
@@ -7,10 +7,15 @@ export const tokenCookieKey = 'kaguyaApp.token';
 export function setupAPIClient(ctx: GetServerSidePropsContext | undefined = undefined) {
   let cookies = parseCookies(ctx);
   
-  const baseURL = process.env.NODE_ENV === 'production' ? 'https://platform-backend-production.up.railway.app' : 'http://localhost:3333';
+  const baseURL = 
+    process.env.NODE_ENV === 'production' ? (
+      process.env.KAGUYA_API_BASE_URL ? 
+      process.env.KAGUYA_API_BASE_URL :
+      'https://platform-backend-production.up.railway.app'
+    ) : 'http://localhost:3333';
   
   const kaguyaApi = axios.create({
-    baseURL: 'https://platform-backend-production.up.railway.app',
+    baseURL,
   });
   
   kaguyaApi.defaults.headers.common.Authorization = `Bearer ${cookies[tokenCookieKey]}`;
